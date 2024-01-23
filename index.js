c6c236ab3c90e23fffc8b32597b4c500f64c5121
Nick McCurdy <nick@nickmccurdy.com>
2024-01-22 21:47:27 -0500

    Switch to JS syntax for comments

---
+++ index.js
@@ -0,0 +1,12 @@
+/** @type import("prettier").Config */
+module.exports = {
+	useTabs: true,
+
+	// Defaults
+	semi: true,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	trailingComma: "all",
+	arrowParens: "always",
+	endOfLine: "lf",
+};
